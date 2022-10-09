33138ab79aa1e3941ec1bdb92846f14a4fef7bbd
mau5mat <mau5mat@gmail.com>
2022-10-09 21:17:42 +0100

    Completed the first part of day2

--- 2021/day2/day2.hs
+++ 2021/day2/day2.hs
@@ -1,11 +1,3 @@
--- Foward, Up, Down from [String]
--- Total Down - Total Up = Total Current Depth
--- Total Forward x Total Current Depth = Answer
-
--- Take every array that contains Forward and make a new array, 
--- flatten it, remove words
--- convert remainding string int to int and sum
-
 main :: IO ()
 main = do
     input <- readFile "input.txt"
@@ -13,15 +5,19 @@ main = do
         forwards = filterPairsWithCommand "forward" listOfTuples
         ups = filterPairsWithCommand "up" listOfTuples
         downs = filterPairsWithCommand "down" listOfTuples
-    print $ concat $ filter (not . null) ups
+        forwards' = sum $ map snd $ filterNullAndFlatten forwards
+        ups' = sum $ map snd $ filterNullAndFlatten ups
+        downs' = sum $ map snd $ filterNullAndFlatten downs
+        answer = forwards' * (downs' - ups')
+    print $ "Answer: " <> show answer
 
 wordsToPairs :: [String] -> [(String, Int)]
 wordsToPairs (x:y:xs) = (x, read y) : wordsToPairs xs
 wordsToPairs (_:_) = error "number of words not divisible by 2"
 wordsToPairs [] = []
 
-total :: [[(String, Int)]] -> Int
-total list = undefined
-
 filterPairsWithCommand :: String -> [[(String, Int)]] -> [[(String, Int)]]
 filterPairsWithCommand command list = (map . filter) (\(a, _) -> a == command) list
+
+filterNullAndFlatten :: [[(String, Int)]] -> [(String, Int)]
+filterNullAndFlatten list = concat $ filter (not . null) list
