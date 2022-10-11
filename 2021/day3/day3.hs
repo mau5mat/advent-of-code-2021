07560c42239c129b8f7dd0309f578c4b253ed354
mau5mat <mau5mat@gmail.com>
2022-10-11 22:54:11 +0100

    Logic for challenge added

--- 2021/day3/day3.hs
+++ 2021/day3/day3.hs
@@ -1,4 +1,9 @@
-module Main (main) where
-
 day3 :: IO ()
-day3 = IO
+day3 = undefined
+
+-- Parse input data from String -> [[Int]]
+-- [[1,1,0],[0,1,0],[0,0,1]] -- Start
+-- --> [[1,0,0],[1,1,0],[0,0,1]] -- New lists from positions of the elements in lists
+-- --> --> [[0],[1],[0]] --> Return 0 or 1 depending on highest precident
+-- --> --> --> 010 --> Flatten to Int
+-- --> --> --> --> 2 --> Convert Int to Binary representation
